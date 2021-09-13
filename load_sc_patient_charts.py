448deea52e56d92be63ca7b23fb0846ba6795140
drew-walkerr <aandrewwalkerr@gmail.com>
2021-09-13 11:37:12 -0400

    updated script to successfully pull SC patient charts, filter by ICD-9 code. Need to still filter down by type of note (remove radiology, etc).

---
+++ load_sc_patient_charts.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+ICDs = pd.read_csv('DIAGNOSES_ICD.csv.gz', compression='gzip',
+    header=0, sep=',', quotechar='"')
+peek_ICDs = ICDs.head()
+print(peek_ICDs)
+ICDs.info()
+is_SCD = ICDs[ICDs['ICD9_CODE'].str.contains('2826',na=False)]
+
+print(is_SCD.head())
+
+is_SCD.info()
+
+patients_unique = is_SCD['SUBJECT_ID'].drop_duplicates()
+
+NOTES = pd.read_csv('NOTEEVENTS.csv.gz', compression='gzip',
+    header=0, sep=',', quotechar='"')
+peek_NOTES = NOTES.head()
+print(peek_NOTES)
+NOTES.info()
+SC_NOTES = NOTES.merge(patients_unique, on = 'SUBJECT_ID')
+print(SC_NOTES['TEXT'])
+print(SC_NOTES.head())
+
+SC_NOTES.info()
+
+PATIENTS = pd.read_csv('PATIENTS.csv.gz', compression='gzip',
+    header=0, sep=',', quotechar='"')
+peek_patients = PATIENTS.head()
+print(peek_patients)
+
+SC_NOTES_PATIENTS = SC_NOTES.merge(PATIENTS, on = 'SUBJECT_ID')
+
+SC_NOTES_PATIENTS.info()
+
+SC_NOTES_PATIENTS.to_csv("SC_NOTES_PATIENTS.csv")
