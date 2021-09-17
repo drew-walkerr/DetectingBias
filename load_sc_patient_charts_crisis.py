88b27bdda7d6d62b71497746ca014389eccc8d87
drew-walkerr <aandrewwalkerr@gmail.com>
2021-09-17 11:39:23 -0400

    added unique files to filter using different methods for ICD 9 codes for SC patients

---
+++ load_sc_patient_charts_crisis.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+ICDs = pd.read_csv('DIAGNOSES_ICD.csv.gz', compression='gzip',
+    header=0, sep=',', quotechar='"')
+peek_ICDs = ICDs.head()
+print(peek_ICDs)
+ICDs.info()
+#Filter for ICD codes for 282.60-282.69, referring to sickle cell types w/wo crisis
+#2824 for thalassemia w + w/o crisis (282.41-282.42)
+is_SCD = ICDs[ICDs['ICD9_CODE'].str.contains('28262|28264|28269|28242', na=False)]
+
+print(is_SCD.head())
+
+is_SCD.info()
+
+patients_unique = is_SCD['SUBJECT_ID'].drop_duplicates()
+
+NOTES = pd.read_csv('NOTEEVENTS.csv.gz', compression='gzip',
+    header=0, sep=',', quotechar='"')
+peek_NOTES = NOTES.head()
+print(peek_NOTES)
+NOTES.info()
+SC_NOTES = NOTES.merge(patients_unique, on = 'SUBJECT_ID')
+print(SC_NOTES['TEXT'])
+print(SC_NOTES.head())
+
+SC_NOTES.info()
+
+PATIENTS = pd.read_csv('PATIENTS.csv.gz', compression='gzip',
+    header=0, sep=',', quotechar='"')
+peek_patients = PATIENTS.head()
+print(peek_patients)
+
+SC_NOTES_PATIENTS = SC_NOTES.merge(PATIENTS, on = 'SUBJECT_ID')
+
+SC_NOTES_PATIENTS.info()
+# Find unique chart types and decide to limit them
+    # Remove Radiology, ECG, Respiratory, Echo notes
+SC_NOTES_PATIENTS_FILTERED = SC_NOTES_PATIENTS[SC_NOTES_PATIENTS["CATEGORY"].str.contains("Radiology|ECG|Respiratory|Echo")==False]
+SC_NOTES_PATIENTS_FILTERED.to_csv("SC_NOTES_PATIENTS_FILTERED.csv")
+
+
+patients_unique_with_notes = SC_NOTES_PATIENTS_FILTERED.nunique()
+# Can we compute how long the text is word-wise?
+# Re run with a column that preserves ICD-09 code in final dataset to double check
+# compare against CF patients?
+# Way to be able to abstract race?
