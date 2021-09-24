55dc8d3ede4a51bfc02f091addd069e58db18e1d
drew-walkerr <aandrewwalkerr@gmail.com>
2021-09-23 22:08:27 -0400

    minor change to get initial tf-idf code to run

--- tf_idf_between_patient_cohorts.py
+++ tf_idf_between_patient_cohorts.py
@@ -5,10 +5,12 @@ import pandas as pd
 CRISIS_PATIENTS = pd.read_csv("SC_NOTES_PATIENTS_CRISIS.csv")
 BROAD_PATIENTS = pd.read_csv("SC_NOTES_PATIENTS_BROAD.csv")
 
+
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 vectorizer = TfidfVectorizer(stop_words='english', ngram_range = (1,1), max_df = .6, min_df = .01)
-X = vectorizer.fit_transform(my_files)
+X = vectorizer.fit_transform(CRISIS_PATIENTS)
 feature_names = vectorizer.get_feature_names()
 dense = X.todense()
 denselist = dense.tolist()
